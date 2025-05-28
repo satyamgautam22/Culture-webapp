5893b0b459f88e8866233231b116b87894c41096
SATYAM GAUTAM <156093741+satyamgautam22@users.noreply.github.com>
2025-05-28 11:45:04 +0530

    Update image path

--- Client/src/Register/Register.jsx
+++ Client/src/Register/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // <-- Import useNavigate
 import './Register.css';
-import loginImage from '../assets/loginImage.png';
+import loginImage from '../assets/LoginImage.png';
 
 const Register = () => {
   const navigate = useNavigate(); // <-- Initialize the navigate function
